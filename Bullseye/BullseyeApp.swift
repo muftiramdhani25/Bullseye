abc950d8da123d2d059fa89398a09de33e900a80
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-09-07 07:34:16 +0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mufti Ramdhani on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
